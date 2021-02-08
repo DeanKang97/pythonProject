0ca1300534c0753b798a94ae21924c7f9d3c4628
Dean Kang <pregea@icloud.com>
2021-02-08 13:31:07 +0900

    made api server

--- api_user/views.py
+++ api_user/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .models import User
+from .serializers import UserSerializer
+from rest_framework import status
 
 # Create your views here.
 
 class UserView(APIView):
-    def get(self, request):
-        return Response("ok", status=200) #테스트용 Response
+    def get(self, request, **kwargs):
+        if kwargs.get('user_id') is None:
+            user_queryset = User.objects.all()  # 모든 User의 정보를 불러온다.
+            user_queryset_serializer = UserSerializer(user_queryset, many=True)
+            return Response(user_queryset_serializer.data, status=status.HTTP_200_OK)
+        else:
+            user_id = kwargs.get('user_id')
+            user_serializer = UserSerializer(User.objects.get(id=user_id))  # id에 해당하는 User의 정보를 불러온다
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        user_serializer = UserSerializer(data=request.data)  # Request의 data를 UserSerializer로 변환
+
+        if user_serializer.is_valid():
+            user_serializer.save()  # UserSerializer의 유효성 검사를 한 뒤 DB에 저장
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)  # client에게 JSON response 전달
+        else:
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        return Response("put_test_ok", status=200)
+
+    def delete(self, request):
+        return Response("delete_test_ok", status=200)
+

c199333cd36ac33f1bb60b51bc9b43110a440844
MongDDangmania <78339038+MongDDangmania@users.noreply.github.com>
2021-03-31 01:06:13 +0900

    Update taegeun_update.py

--- Myeongha/taegeun_update.py
+++ Myeongha/taegeun_update.py
@@ -1,5 +1,5 @@
 import requests
-
+import time
 url = "https://openapi.naver.com/v1/search/blog?query="  # 네이버 검색 api
 
 client_id = "iq6LEjpJhnsJLV5ev7_a"
@@ -12,10 +12,10 @@ header = {"X-Naver-Client-Id": client_id,  # api_key and id
 
 def search_link(search_term):
     rs = requests.get(url + search_term , headers=header)
-    tmp = rs.json()
-    #print(tmp['items'])
+    time.sleep(0.1) #에러를 줄이기 위한 sleep
+    tmp = rs.json() #json화
     total = tmp['total'] #총 검색량
-    print("총 검색량: " + str(total))
+    print("첫 총 검색량: " + str(total))
     if total > 1000: #start가 1000이 넘지 않게 하기 위한 장치
         total = 999
     links = list()  # 링크들 저장할 주소
@@ -24,17 +24,18 @@ def search_link(search_term):
         print("error")
         return -1
 
-
-
     for i in range(0,int(total/100+1)): #0~백단위+1까지
         start = i*100+1 # start는 1000이 최대ㅠ
         rs = requests.get(url + search_term + "&&display=100&&start=" + str(start), headers=header)
+        time.sleep(0.1) #에러를 줄이기 위한 sleep
         tmp1 = rs.json()
-        for j in range(0, 100):
-            links.append(tmp1['items'][j]['link'])  # link만 찾아 links에 저
+        for j in range(0, len(tmp1['items'])):
+            links.append(tmp1['items'][j]['link'])  # link만 찾아 links에 저장
 
     return links  # 링크가 저장된 배열 리턴
 
-print(len(search_link("룰429")))
+print("총 리스트 길이: "+ str(len(search_link("라로슈포제 시카플라스트밤"))))
 
-print(search_link("룰429"))
+#블로그링크 리스트 생성
+blog_link = search_link("라로슈포제 시카플라스트밤")
+
